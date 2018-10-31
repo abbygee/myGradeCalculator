3ef07c911873587f11a0b07629bb6e271c9a2878
abbygee <abigaildelgado@students.berkeley.net>
2018-10-31 14:27:01 -0700

    ahhh

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var count = 0;
 
+function calculateGradeNeeded(){
+    var cur = calculateCurrentGrade();
+    var desire = document.getElementById('wanted').value;
+    var weight = document.getElementById('finalweight').value;
+    parseInt(desire);
+    parseInt(weight);
+    weight = weight/100;
+
+    var needed = (desire - cur * (1 - weight)) / weight;
+
+   document.getElementById('needed').innerHTML = needed.toString().slice(0,4);
+}
+
 function calculateCurrentGrade(){
     /* This calculates the first row which is required for the site */
     var cat1 = document.getElementById('ipoints').value;
@@ -22,7 +35,9 @@ function calculateCurrentGrade(){
     }
     constant += cat1final;
 
-    document.getElementById('test').innerHTML = constant.toString().slice(0,4) + "%";
+    document.getElementById('grade').innerHTML = constant.toString().slice(0,4) + "%";
+
+    return constant;
 }
 
 function averageArray(array){
@@ -37,7 +52,6 @@ function averageArray(array){
     return avg;
 }
 
-
 function convertArrayStringToNumber(string){
     var grades = string.split(",");
 
@@ -46,7 +60,6 @@ function convertArrayStringToNumber(string){
     }
     return grades;
 }
-
 
 function addRow() {
     if(count <= 4){
@@ -57,7 +70,7 @@ function addRow() {
         /* value columns */
         var col1= document.createElement('td');
         var col2 = document.createElement('td');
-        /* headings */
+        /* heading columns */
         var col3 = document.createElement('th');
         var col4 = document.createElement('th');
         /* text inputs */
@@ -80,8 +93,8 @@ function addRow() {
             valueRow.appendChild(col1);
             valueRow.appendChild(col2);
 
-            document.getElementById('container').appendChild(labelRow);
-            document.getElementById('container').appendChild(valueRow);
+            document.getElementById('table1').appendChild(labelRow);
+            document.getElementById('table1').appendChild(valueRow);
             count++;
         }else{
             alert("Please enter a category name");
